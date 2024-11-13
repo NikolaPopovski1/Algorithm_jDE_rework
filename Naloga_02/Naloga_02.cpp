127c55df1c43edb23e3c880047240dc6fa76fd2e
N1cK <nikola.popovski@student.um.si>
2024-11-13 15:29:39 +0100

    Task 2
    Working parallel calculations

--- Naloga_02/Naloga_02.cpp
+++ Naloga_02/Naloga_02.cpp
@@ -8,12 +8,16 @@
 #ifndef M_PI
 #define M_PI 3.1415926f
 #endif
-
-
-// Constants
+#include <thread>
 constexpr const float XjL = -(float)M_PI;
 constexpr const float XjU = (float)M_PI;
 constexpr const float epsilon = (float)1e-6;
+
+std::string aminoAcids;
+static unsigned int seed, nFesLmt, runtimeLmt, expRuns, np, expThreads;
+static std::atomic<int> atomicInt = 0;
+static float target, F, Cr;
+std::vector<std::tuple<int, int, float, float>> results;
 
 struct Solution {
     std::vector<float> xThetas;
@@ -22,7 +26,6 @@ struct Solution {
     float F;
     float Cr;
 };
-
 struct Coordinates {
     float x, y, z;
 
@@ -45,14 +48,12 @@ Coordinates coordinate(std::vector<Coordinates>& allCoords, Solution& element, i
         throw std::runtime_error("Index for calculating coordinate not valid...");
     }
 }
-
 float distance(Coordinates& ri, Coordinates& rj) {
     float first = rj.x - ri.x;
     float second = rj.y - ri.y;
     float third = rj.z - ri.z;
     return sqrtf(first * first + second * second + third * third);
 }
-
 constexpr float c(char& aminoAcidI, char& aminoAcidJ) {
     if (aminoAcidI == 'A' && aminoAcidJ == 'A') return 1.f;
     if (aminoAcidI == 'B' && aminoAcidJ == 'B') return 0.5f;
@@ -60,7 +61,6 @@ constexpr float c(char& aminoAcidI, char& aminoAcidJ) {
 
     throw std::runtime_error("Invalid amino acid pair: " + std::string(1, aminoAcidI) + " and " + std::string(1, aminoAcidJ));
 }
-
 float energyCalculation(std::string& aminoAcids, Solution& element) {
     float resultOne = 0.f;
     for (int i = 0; i < element.xThetas.size(); i++) {
@@ -87,12 +87,150 @@ float energyCalculation(std::string& aminoAcids, Solution& element) {
     return resultOne + resultTwo * 4.f;
 }
 
-int main(int argc, char* argv[]) {
-    std::string aminoAcids;
-    unsigned int seed, nFesLmt, runtimeLmt, np;
-    unsigned int nFesCounter = 0;
-    float target;
+void calculateSeedEnergy() {
+    std::atomic<unsigned int> bestEnergy, nFesCounter;
+    Solution sol;
+    Solution* populationCurrGen = new Solution[np];
+    std::uniform_real_distribution<float> float_dist(0.f, 1.f);
+    std::uniform_int_distribution<int> int_dist_np(0, static_cast<int>(np - 1));
+    std::uniform_int_distribution<int> int_dist_d(0, static_cast<int>(aminoAcids.size() - 1));
 
+    while (true) {
+        int currentSeed = atomicInt.fetch_add(1);
+        if (currentSeed > expRuns) break;
+
+        unsigned int bestEnergy = nFesCounter = 0;
+        std::mt19937 gen(currentSeed + seed);
+        auto randFloat0To1 = [&]() { return float_dist(gen); };
+        auto randFloatNp = [&]() { return int_dist_np(gen); };
+        auto randIntD = [&]() { return int_dist_d(gen); };
+
+        auto start = std::chrono::high_resolution_clock::now();
+        auto now = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+        for (unsigned int i = 0; i < np; i++) {
+            sol = Solution();
+            sol.F = 0.5f;
+            sol.Cr = 0.9f;
+
+            sol.xThetas.push_back(XjL + 2 * XjU * randFloat0To1());
+            for (int j = 3; j < aminoAcids.size(); j++) {
+                sol.xThetas.push_back(XjL + 2 * XjU * randFloat0To1());
+                sol.xBetas.push_back(XjL + 2 * XjU * randFloat0To1());
+            }
+
+            /* // for testing
+            sol.xThetas = {0.7556,  0.0503, -0.8505,  0.0011,  0.2203,  1.1535, -0.1118,  0.1564,    0.1536,  0.0390,  1.2929};
+            sol.xBetas = { -0.1156,  0.0230, -1.8169,  2.7985, -3.0959,  -0.3611,  0.4678,  2.2303,  2.9020,  0.1797};
+            */
+
+            sol.energy = energyCalculation(aminoAcids, sol);
+            populationCurrGen[i] = sol;
+            nFesCounter++;
+        }
+        Solution u, uNew;
+        unsigned int r1, r2, r3, rBest, jRand;
+        rBest = 0;
+        float tmp;
+        while (elapsed_ms <= runtimeLmt && nFesCounter <= nFesLmt) {
+            now = std::chrono::high_resolution_clock::now();
+            elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+            for (unsigned int i = 0; i < np; i++) {
+                u = Solution();
+
+                float randomValue = randFloat0To1();
+                if (randomValue < 0.1) F = 0.1f + 0.9f * randomValue;
+                else F = populationCurrGen[i].F;
+
+                randomValue = randFloat0To1();
+                if (randomValue < 0.1) Cr = randomValue;
+                else Cr = populationCurrGen[i].Cr;
+
+                r1 = r2 = r3 = i;
+                while (r1 == i) r1 = randFloatNp();
+                while (r2 == i || r2 == r1) r2 = randFloatNp();
+                while (r3 == i || r3 == r1 || r3 == r2) r3 = randFloatNp();
+
+                jRand = randIntD();
+                // then go through for all thetas
+                for (int j = 0; j < aminoAcids.size() - 2; j++) {
+                    if (randFloat0To1() < Cr || j == jRand) {
+                        u.xThetas.push_back(populationCurrGen[r3].xThetas[j] + F * (populationCurrGen[r1].xThetas[j] - populationCurrGen[r2].xThetas[j]));
+                        if (u.xThetas[j] <= XjL) u.xThetas[j] = 2 * XjU + u.xThetas[j];
+                        if (u.xThetas[j] > XjU) u.xThetas[j] = 2 * XjL + u.xThetas[j];
+                    }
+                    else u.xThetas.push_back(populationCurrGen[i].xThetas[j]);
+                }
+                // then go through for all betas
+                for (int j = 0; j < aminoAcids.size() - 3; j++) {
+                    if (randFloat0To1() < Cr || j == jRand) {
+                        u.xBetas.push_back(populationCurrGen[r3].xBetas[j] + F * (populationCurrGen[r1].xBetas[j] - populationCurrGen[r2].xBetas[j]));
+                        if (u.xBetas[j] <= XjL) u.xBetas[j] = 2 * XjU + u.xBetas[j];
+                        if (u.xBetas[j] > XjU) u.xBetas[j] = 2 * XjL + u.xBetas[j];
+                    }
+                    else u.xBetas.push_back(populationCurrGen[i].xBetas[j]);
+                }
+                u.energy = energyCalculation(aminoAcids, u);
+                nFesCounter++;
+
+                if (u.energy <= populationCurrGen[i].energy) {
+                    uNew = Solution();
+                    for (int j = 0; j < aminoAcids.size() - 2; j++) {
+                        uNew.xThetas.push_back(populationCurrGen[r3].xThetas[j] + 0.5f * (u.xThetas[j] - populationCurrGen[i].xThetas[j]));
+                        if (uNew.xThetas[j] <= XjL) uNew.xThetas[j] = 2 * XjU + uNew.xThetas[j];
+                        if (uNew.xThetas[j] > XjU) uNew.xThetas[j] = 2 * XjL + uNew.xThetas[j];
+                    }
+                    for (int j = 0; j < aminoAcids.size() - 3; j++) {
+                        uNew.xBetas.push_back(populationCurrGen[r3].xBetas[j] + 0.5f * (u.xBetas[j] - populationCurrGen[i].xBetas[j]));
+                        if (uNew.xBetas[j] <= XjL) uNew.xBetas[j] = 2 * XjU + uNew.xBetas[j];
+                        if (uNew.xBetas[j] > XjU) uNew.xBetas[j] = 2 * XjL + uNew.xBetas[j];
+                    }
+
+                    uNew.energy = energyCalculation(aminoAcids, uNew);
+                    nFesCounter++;
+
+                    if (uNew.energy <= u.energy) {
+                        populationCurrGen[i].xThetas = uNew.xThetas;
+                        populationCurrGen[i].xBetas = uNew.xBetas;
+                        populationCurrGen[i].energy = uNew.energy;
+                        populationCurrGen[i].Cr = Cr;
+                        populationCurrGen[i].F = F;
+                    }
+                    else {
+                        populationCurrGen[i].xThetas = u.xThetas;
+                        populationCurrGen[i].xBetas = u.xBetas;
+                        populationCurrGen[i].energy = u.energy;
+                    }
+                }
+            }
+
+            tmp = populationCurrGen[0].energy;
+            for (unsigned int j = 1; j < np; j++) {
+                if (tmp > populationCurrGen[j].energy) {
+                    rBest = j;
+                    tmp = populationCurrGen[j].energy;
+                }
+            }
+            bestEnergy = rBest;
+
+            if (populationCurrGen[rBest].energy <= target + epsilon && populationCurrGen[rBest].energy >= target + epsilon) break;
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        //std::cout << currentSeed + seed << " " << populationCurrGen[bestEnergy].energy << " " << nFesCounter << " " << duration << '\n';
+        //std::cout << "Speed (nFes / duration = speed): " << nFesCounter << " / " << duration << " = " << nFesCounter/duration << '\n';
+
+
+        std::tuple<int, int, float, float> seedVals(currentSeed + seed, nFesCounter, populationCurrGen[bestEnergy].energy, duration);
+        results.push_back(seedVals);
+    }
+
+    delete[] populationCurrGen;
+}
+
+int main(int argc, char* argv[]) {
     try {
         for (int i = 1; i < argc; i++) {
             std::string arg = argv[i];
@@ -102,6 +240,8 @@ int main(int argc, char* argv[]) {
             else if (arg == "-nfesLmt") nFesLmt = std::stoi(argv[++i]);
             else if (arg == "-runtimeLmt") runtimeLmt = std::stoi(argv[++i]);
             else if (arg == "-Np" || arg == "-np") np = std::stoi(argv[++i]);
+            else if (arg == "-expRuns") expRuns = std::stoi(argv[++i]);
+            else if (arg == "-expThreads") expThreads = std::stoi(argv[++i]);
         }
 
         if (aminoAcids.empty()) throw std::runtime_error("Error: 'aminoAcids' not initialized.");
@@ -110,6 +250,8 @@ int main(int argc, char* argv[]) {
         if (!nFesLmt) throw std::runtime_error("Error: 'nFesLmt' not initialized.");
         if (!runtimeLmt) throw std::runtime_error("Error: 'runtimeLmt' not initialized.");
         if (!np) throw std::runtime_error("Error: 'np' not initialized.");
+        if (!expRuns) throw std::runtime_error("Error: 'expRuns' not initialized.");
+        if (!expThreads) throw std::runtime_error("Error: 'expThreads' not initialized.");
 
     }
     catch (const std::exception& e) {
@@ -117,146 +259,31 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-
-    std::mt19937 gen(seed);
-
-    std::uniform_real_distribution<float> float_dist(0.f, 1.f);
-    auto randFloat0To1 = [&]() { return float_dist(gen); };
-
-    std::uniform_int_distribution<int> int_dist_np(0, static_cast<int>(np - 1));
-    auto randFloatNp = [&]() { return int_dist_np(gen); };
-
-    std::uniform_int_distribution<int> int_dist_d(0, static_cast<int>(aminoAcids.size() - 1));
-    auto randIntD = [&]() { return int_dist_d(gen); };
-
-    unsigned int bestEnergy = 0;
-    float F, Cr;
-
     auto start = std::chrono::high_resolution_clock::now();
-    auto now = std::chrono::high_resolution_clock::now();
-    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
 
-    Solution sol;
-    Solution* populationCurrGen = new Solution[np];
-    for (unsigned int i = 0; i < np; i++) {
-        sol = Solution();
-        sol.F = 0.5f;
-        sol.Cr = 0.9f;
-
-        sol.xThetas.push_back(XjL + 2 * XjU * randFloat0To1());
-        for (int j = 3; j < aminoAcids.size(); j++) {
-            sol.xThetas.push_back(XjL + 2 * XjU * randFloat0To1());
-            sol.xBetas.push_back(XjL + 2 * XjU * randFloat0To1());
-        }
-
-        /* // for testing
-        sol.xThetas = {0.7556,  0.0503, -0.8505,  0.0011,  0.2203,  1.1535, -0.1118,  0.1564,    0.1536,  0.0390,  1.2929};
-        sol.xBetas = { -0.1156,  0.0230, -1.8169,  2.7985, -3.0959,  -0.3611,  0.4678,  2.2303,  2.9020,  0.1797};
-        */
-
-        sol.energy = energyCalculation(aminoAcids, sol);
-        populationCurrGen[i] = sol;
-        nFesCounter++;
+    std::vector<std::thread> threads;
+    for (int i = 0; i < expThreads; i++) {
+        threads.push_back(std::thread(calculateSeedEnergy));
     }
 
-    Solution u, uNew;
-    unsigned int r1, r2, r3, rBest, jRand;
-    rBest = 0;
-    float tmp;
-    while (elapsed_ms <= runtimeLmt && nFesCounter <= nFesLmt) {
-        now = std::chrono::high_resolution_clock::now();
-        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
-
-        for (unsigned int i = 0; i < np; i++) {
-            u = Solution();
-
-            float randomValue = randFloat0To1();
-            if (randomValue < 0.1) F = 0.1f + 0.9f * randomValue;
-            else F = populationCurrGen[i].F;
-
-            randomValue = randFloat0To1();
-            if (randomValue < 0.1) Cr = randomValue;
-            else Cr = populationCurrGen[i].Cr;
-
-            r1 = r2 = r3 = i;
-            while (r1 == i) r1 = randFloatNp();
-            while (r2 == i || r2 == r1) r2 = randFloatNp();
-            while (r3 == i || r3 == r1 || r3 == r2) r3 = randFloatNp();
-
-            jRand = randIntD();
-            // then go through for all thetas
-            for (int j = 0; j < aminoAcids.size() - 2; j++) {
-                if (randFloat0To1() < Cr || j == jRand) {
-                    u.xThetas.push_back(populationCurrGen[r3].xThetas[j] + F * (populationCurrGen[r1].xThetas[j] - populationCurrGen[r2].xThetas[j]));
-                    if (u.xThetas[j] <= XjL) u.xThetas[j] = 2 * XjU + u.xThetas[j];
-                    if (u.xThetas[j] > XjU) u.xThetas[j] = 2 * XjL + u.xThetas[j];
-                }
-                else u.xThetas.push_back(populationCurrGen[i].xThetas[j]);
-            }
-            // then go through for all betas
-            for (int j = 0; j < aminoAcids.size() - 3; j++) {
-                if (randFloat0To1() < Cr || j == jRand) {
-                    u.xBetas.push_back(populationCurrGen[r3].xBetas[j] + F * (populationCurrGen[r1].xBetas[j] - populationCurrGen[r2].xBetas[j]));
-                    if (u.xBetas[j] <= XjL) u.xBetas[j] = 2 * XjU + u.xBetas[j];
-                    if (u.xBetas[j] > XjU) u.xBetas[j] = 2 * XjL + u.xBetas[j];
-                }
-                else u.xBetas.push_back(populationCurrGen[i].xBetas[j]);
-            }
-            u.energy = energyCalculation(aminoAcids, u);
-            nFesCounter++;
-
-            if (u.energy <= populationCurrGen[i].energy) {
-                uNew = Solution();
-                for (int j = 0; j < aminoAcids.size() - 2; j++) {
-                    uNew.xThetas.push_back(populationCurrGen[r3].xThetas[j] + 0.5f * (u.xThetas[j] - populationCurrGen[i].xThetas[j]));
-                    if (uNew.xThetas[j] <= XjL) uNew.xThetas[j] = 2 * XjU + uNew.xThetas[j];
-                    if (uNew.xThetas[j] > XjU) uNew.xThetas[j] = 2 * XjL + uNew.xThetas[j];
-                }
-                for (int j = 0; j < aminoAcids.size() - 3; j++) {
-                    uNew.xBetas.push_back(populationCurrGen[r3].xBetas[j] + 0.5f * (u.xBetas[j] - populationCurrGen[i].xBetas[j]));
-                    if (uNew.xBetas[j] <= XjL) uNew.xBetas[j] = 2 * XjU + uNew.xBetas[j];
-                    if (uNew.xBetas[j] > XjU) uNew.xBetas[j] = 2 * XjL + uNew.xBetas[j];
-                }
-
-                uNew.energy = energyCalculation(aminoAcids, uNew);
-                nFesCounter++;
-
-                if (uNew.energy <= u.energy) {
-                    populationCurrGen[i].xThetas = uNew.xThetas;
-                    populationCurrGen[i].xBetas = uNew.xBetas;
-                    populationCurrGen[i].energy = uNew.energy;
-                    populationCurrGen[i].Cr = Cr;
-                    populationCurrGen[i].F = F;
-                }
-                else {
-                    populationCurrGen[i].xThetas = u.xThetas;
-                    populationCurrGen[i].xBetas = u.xBetas;
-                    populationCurrGen[i].energy = u.energy;
-                }
-            }
-        }
-
-        tmp = populationCurrGen[0].energy;
-        for (unsigned int j = 1; j < np; j++) {
-            if (tmp > populationCurrGen[j].energy) {
-                rBest = j;
-                tmp = populationCurrGen[j].energy;
-            }
-        }
-        bestEnergy = rBest;
-
-        if (populationCurrGen[rBest].energy <= target + epsilon && populationCurrGen[rBest].energy >= target + epsilon) break;
+    for (auto& t : threads) {
+        t.join();
     }
+
     auto end = std::chrono::high_resolution_clock::now();
-
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << populationCurrGen[bestEnergy].energy << '\n';
-    std::cout << nFesCounter << '\n';
-    std::cout << duration << '\n';
-    //std::cout << "Speed (nFes / duration = speed): " << nFesCounter << " / " << duration << " = " << nFesCounter/duration << '\n';
-    delete[] populationCurrGen;
+    std::cout << "Duration for all 50 seeds using " << expThreads << " threads:" << duration << std::endl;
 
+    for (const auto& result : results) {
+        int firstInt = std::get<0>(result);
+        int secondInt = std::get<1>(result);
+        float firstFloat = std::get<2>(result);
+        float secondFloat = std::get<3>(result);
 
-
+        // Print the values
+        std::cout << "Seed: " << firstInt << ", nFes: " << secondInt << ", energy: "
+            << firstFloat << ", duration: " << secondFloat << "\n";
+    }
+   
     return 0;
 }
---
+++ multi_seed_jDE.py
@@ -0,0 +1,61 @@
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Lists to store energy, nfes, and duration
+energies = []
+runtimes = []
+nfes_list = []
+
+# Run the program for 50 seeds
+for seed in range(1, 51):
+    result = subprocess.run([f'.\\Naloga_02.exe', "ABBBBBBABBBAB", "-seed", str(seed), "-target", "-5.6104", 
+                             "-nfesLmt", "1000000", "-Np", "300", "-runtimeLmt", "3600"], capture_output=True, text=True)
+    
+    # Extract the values by splitting the output
+    output_lines = result.stdout.split('\n')
+    if len(output_lines) >= 3:
+        energy = float(output_lines[0])
+        nfes = int(output_lines[1])
+        duration = float(output_lines[2])
+        
+        # Append values to lists
+        energies.append(energy)
+        nfes_list.append(nfes)
+        runtimes.append(duration)
+    else:
+        print(f"Unexpected output format for seed {seed}")
+
+# Calculate statistics
+best_energy = min(energies)
+worst_energy = max(energies)
+average_energy = np.mean(energies)
+std_dev_energy = np.std(energies)
+average_runtime = np.mean(runtimes)
+average_nfes = np.mean(nfes_list)
+
+# Output results
+print(f"Best energy: {best_energy}")
+print(f"Worst energy: {worst_energy}")
+print(f"Average energy: {average_energy}")
+print(f"Standard deviation of energy: {std_dev_energy}")
+print(f"Average runtime: {average_runtime} ms")
+print(f"Average nfes: {average_nfes}")
+
+# Plot all graphs in subplots
+plt.figure(figsize=(15, 10))
+
+plt.subplot(3, 1, 1)
+plt.plot(energies)
+plt.ylabel('Energies')
+
+plt.subplot(3, 1, 2)
+plt.plot(runtimes)
+plt.ylabel('Runtimes')
+
+plt.subplot(3, 1, 3)
+plt.plot(nfes_list)
+plt.ylabel('NFES')
+
+plt.tight_layout()
+plt.show()
